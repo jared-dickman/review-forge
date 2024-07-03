99eb08f620f3c86f04ef0ffc453873fba716afb8
jdickman <zenador15@gmail.com>
2024-07-03 11:26:01 -0400

    set pr link from url params

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,18 @@
+import {useEffect} from 'react'
 import {AppContent} from 'src/components/AppContent.tsx'
 import {AppNavigation} from 'src/components/AppNavigation.tsx'
 import 'src/styles/app.css'
+import {useReviewStore} from 'src/stores/ReviewStore.ts'
 
 function App() {
+  const { setLink } = useReviewStore()
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search)
+    const prUrl = urlParams.get('prUrl')
+    if (prUrl) setLink(prUrl)
+  }, [])
+
   return (<>
     <AppNavigation/>
     <AppContent/>
